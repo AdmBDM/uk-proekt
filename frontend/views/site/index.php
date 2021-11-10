6753b17c834f70fb67d2ea1cc035f9bc6c49bbed
Jim7rfp <Jim7rfp@gmail.com>
2021-11-10 19:09:08 +0300

    10.11.2021 Сформирован блок "About"

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -3,17 +3,10 @@
 /* @var $this yii\web\View */
 
 //Yii::$app->name = 'УК Проект';
-//use yii\bootstrap4\Carousel;
 use yii\helpers\Html;
 
 Yii::$app->name = 'ООО «УК «Проект»';
 $this->title = Yii::$app->name;
-
-//$images[] = Html::a(
-//	Html::img($item->getUrl('x200'), ['alt' => '']),
-//	[$item->getUrl()],
-//	['data-method' => 'post', 'target' => '_blank']
-//);
 
 ?>
 <div class="site-index">
@@ -21,7 +14,6 @@ $this->title = Yii::$app->name;
 	<div id="main-slider" class="owl-carousel owl-theme">
 		<div class="item">
 			<div class="item-text-half_left">
-<!--				<h2 id="headline-16-8" class="ct-headline">Приветствуем Вас на сайте ООО «УК «Проект»</h2>-->
 				<div class="slide-1">
 					<h2>Приветствуем Вас на сайте ООО «УК «Проект»</h2>
 					<h3>в разделе меню "Раскрытие информации" Вы можете найти раскрываемую информацию в соответствии с Указанием Банка России № 5609-У</h3>
@@ -42,8 +34,10 @@ $this->title = Yii::$app->name;
 		</div>
 	</div>
 
+	<?= $this->render('about')?>
 
 	<div class="content">
+
 
 	</div>
 </div>
