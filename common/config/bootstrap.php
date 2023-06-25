1ae309f79e11fb5c2e5556b53e1b84bf96f6e6ac
Dmitriy Belik <Jim7rfp@gmail.com>
2023-06-25 10:20:19 +0300

    25.06.2023 Компоновка вывода "Раскрытия информации"

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -1,5 +1,7 @@
 <?php
 Yii::setAlias('@common', dirname(__DIR__));
-Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
-Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
-Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
+Yii::setAlias('@frontend', dirname(__DIR__, 2) . '/frontend');
+Yii::setAlias('@backend', dirname(__DIR__, 2) . '/backend');
+Yii::setAlias('@console', dirname(__DIR__, 2) . '/console');
+Yii::setAlias('@com_images', dirname(__DIR__, 2) . '/images');
+Yii::setAlias('@com_files', dirname(__DIR__, 2) . '/ukpfiles');
